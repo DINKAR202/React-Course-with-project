3109afed5424be9ec4d0889de9e751b9ace9aaf5
DINKAR202 <Dinkar202@outlook.com>
2023-10-21 12:11:12 +0530

    upload commit

--- src/components/Hooks/Colors.jsx
+++ src/components/Hooks/Colors.jsx
@@ -8,11 +8,24 @@ const Colors = () => {
       <h1>This is my Favorite {color} color!</h1>
 
       <div className="container border w-100 d-flex justify-content-around">
-        <button className="btn btn-secondary onClick={() => setColor("blue")}">Click Me</button>
-        <button className="btn btn-warning">Click Me</button>
-        <button className="btn btn-primary">Click Me</button>
-        <button className="btn btn-success">Click Me</button>
-        <button className="btn btn-danger">Click Me</button>
+        <button
+          className="btn btn-secondary"
+          onClick={() => setColor("Secondary")}
+        >
+          Click Me
+        </button>
+        <button className="btn btn-warning" onClick={() => setColor("Warning")}>
+          Click Me
+        </button>
+        <button className="btn btn-primary" onClick={() => setColor("Primary")}>
+          Click Me
+        </button>
+        <button className="btn btn-success" onClick={() => setColor("Success")}>
+          Click Me
+        </button>
+        <button className="btn btn-danger" onClick={() => setColor("Danger")}>
+          Click Me
+        </button>
       </div>
     </>
   );
