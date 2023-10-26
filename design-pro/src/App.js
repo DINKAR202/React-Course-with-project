0ed509a8c39baa919077de71b83a5e0ab1b17438
DINKAR202 <Dinkar202@outlook.com>
2023-10-27 01:42:28 +0530

    first commit

---
+++ design-pro/src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner";
+import NavScrollExample from "./components/NavScrollExample";
+
+function App() {
+  return (
+    <>
+        <NavScrollExample/>
+        <Banner/>
+    </>
+  );
+}
+
+export default App;
