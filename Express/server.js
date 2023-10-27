e88498c3cd47fa6c0def2d8024ff3589576cae05
DINKAR202 <Dinkar202@outlook.com>
2023-10-27 23:41:37 +0530

    first commit

--- Express/server.js
+++ Express/server.js
@@ -6,13 +6,13 @@ const PORT = process.env.PORT || 9000;
 
 
 app.use(express.static('public'));
-// app.get('/', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname) + '/index.html');
-// });
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname) + '/index.html');
+});
 
-// app.get('/about', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname) + '/about.html');
-// });
+app.get('/about', (req, res) =>{
+    res.sendFile(path.resolve(__dirname) + '/about.html');
+});
 
 
 app.listen(PORT, () => {
