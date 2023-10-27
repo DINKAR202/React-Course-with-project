66f560044fe9e6bbbf5695a99e1fbc6ecde0f3af
DINKAR202 <Dinkar202@outlook.com>
2023-10-27 21:19:44 +0530

    first commit

--- Express/server.js
+++ Express/server.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
+
+
+app.get('/', (req, res) =>{
+    res.send('Hello from Express js........');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listing app is ${PORT}`);
