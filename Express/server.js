22f6e274f4189229dcfa84b43e991b8cc895920e
DINKAR202 <Dinkar202@outlook.com>
2023-10-27 21:52:47 +0530

    first commit

--- Express/server.js
+++ Express/server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname) + '/index.html');
 });
 
+app.get('/about', (req, res) =>{
+    res.sendFile(path.resolve(__dirname) + '/about.html');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listing app is ${PORT}`);
