9544ec451269743ffa5ec893942009809ac15acb
DINKAR202 <Dinkar202@outlook.com>
2023-10-28 22:33:57 +0530

    first commit

--- Express/middleware/apiKey.js
+++ Express/middleware/apiKey.js
@@ -1,6 +1,9 @@
 function apiKey(req, res, next) {
     const api_key = '1234567';
-    console.log(req.query) 
+    console.log(req.query.api_key);
+    if(req.query.api_key && ){
+
+    } 
     next();
 }
 
