692ceeab8496c9a813184a96449c0c1809dc0520
DINKAR202 <Dinkar202@outlook.com>
2023-10-28 22:43:09 +0530

    first commit

--- Express/middleware/apiKey.js
+++ Express/middleware/apiKey.js
@@ -1,7 +1,9 @@
 function apiKey(req, res, next) {
     const api_key = '1234567';
-    console.log(req.query.api_key);
-    if(req.query.api_key && (req.query.api_key == api_key)){
+    // console.log();
+    const userApiKey = req.query.api_key;
+
+    if(userApiKey && (userApiKey == api_key)){
         next();
     } else{
         res.json({message: 'Not Allowed!'});
