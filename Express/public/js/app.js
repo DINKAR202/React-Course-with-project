3a1e9b56d4b302d61004e12b0d94e41fb6b4364a
DINKAR202 <Dinkar202@outlook.com>
2023-10-31 22:22:06 +0530

    first commit

--- Express/public/js/app.js
+++ Express/public/js/app.js
@@ -17,7 +17,7 @@ const App = () => {
     <ul className="list-group">
       {products.map((product) => {
         return (
-          <li key={product.id} className="list-group-item">
+          <li key={product.id} className="list-group-item d-flex justify-content-between align-items-center">
             <div>
               <strong>{product.name}: </strong>${product.price}
             </div>
