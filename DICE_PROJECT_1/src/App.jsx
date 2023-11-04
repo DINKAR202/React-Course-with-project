fbf5148d1e4994e4fd62096c9ac09baac459abbb
DINKAR202 <Dinkar202@outlook.com>
2023-11-04 13:24:22 +0530

    updated commit

--- DICE_PROJECT_1/src/App.jsx
+++ DICE_PROJECT_1/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
 
 function App(){
-  return(
-    <>
-      <StartGame />
-      
-    </>
-  )
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  }
+
+  return <>{isGameStarted ? <GamePlay /> : <StartGame  toggle={toggleGamePlay} />}</>;
 }
 
 export default App;
