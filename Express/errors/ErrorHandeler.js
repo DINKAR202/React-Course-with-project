b76982165df11c2bbf8e0f072228d072a2f7b66b
DINKAR202 <Dinkar202@outlook.com>
2023-11-10 23:02:28 +0530

    updated commit

--- Express/errors/ErrorHandeler.js
+++ Express/errors/ErrorHandeler.js
@@ -1,3 +1,16 @@
 const ErrorHandeler {
+    constructor(status, msg){
+        this.status = status;
+        this.message = msg;
+    }
+
+
+    static validationError(message = "All fields are required!"){
+        return new ErrorHandeler(422, message);            
+    }
+
+    static notFoundError(message = 'Not found!'){
+        return new ErrorHandeler(404, message);            
+    }
     
 }
