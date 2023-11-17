a5c33321b3543713a008b69b7542fc70a8c52101
DINKAR202 <Dinkar202@outlook.com>
2023-11-18 00:18:40 +0530

    updated commit

--- login/server/routes/auth.js
+++ login/server/routes/auth.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 const {User} = require("../models/User");
 const joi = require("joi");
+const bcrypt = require("bcrypt");
+
 
 router.post('/', async(req, res) => {
     try {
+         const { error } = validate(req.body);
+         if(error)
+            return res.status(400).send({message: error.details[0].message});
+        const user = await User.findOne({email: req.body.email});
+        if(!user)
+            return res.status(401).send({message:"Invalid Email or password"});
         
+        const validPassword = await bcrypt.compare(
+            req.body.password, user.password
+        );
+            
     } catch (error) {
         
     }
