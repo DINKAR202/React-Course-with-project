aec8d0f68346ca68562f558afccab6933b82beee
DINKAR202 <Dinkar202@outlook.com>
2023-11-18 01:02:09 +0530

    updated commit

--- login/server/index.js
+++ login/server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connection = require("./db")
+const userRouter = require('./routes/users');
+const authRoutes = require('./routes/auth')
 
 
 // database connection
@@ -11,6 +13,10 @@ connection();
 // middleware
 app.use(express.json())
 app.use(cors());
+
+// routes
+app.use("/api/users", userRouters);
+app.use("/api/auth", authRoutes);
 
 
 const port = process.env.PORT || 8080;
