dac7c021fe4c986fefc4b1b2d28e4adea9387a6a
DINKAR202 <Dinkar202@outlook.com>
2023-11-18 22:09:30 +0530

    updated commit

--- login/client/src/components/Signup/index.jsx
+++ login/client/src/components/Signup/index.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import styles from './styles.module.css';
 import {Link} from 'react-router-dom';
 
 const Signup = () =>{
+    const [data, setData] = useState({
+        firstName: "",
+        lastName: "",
+        email:"",
+        password:"",
+    });
+
+    const handleChange = ({currentTarget:input}) => {
+        setData({...data, [input.name]: input.value});
+    }
+
+
     return(
             <div className={styles.signup_container}>
                 <div className={styles.signup_form_container}>
@@ -17,8 +30,12 @@ const Signup = () =>{
                       <form className={styles.form_container}>
                             <h1>Create Account</h1>
                             <input type="text" 
-                            placeholder='' 
-                            id="" 
+                            placeholder='First Name' 
+                            name='firstName'
+                            onChange={handleChange}
+                            value={data.firstName}
+                            required
+                            className={styles.input}
                             />
                         </form>          
                     </div>
