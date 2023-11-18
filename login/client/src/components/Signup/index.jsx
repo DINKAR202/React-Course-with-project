00f9d19a56e9c8a6c1e038ce8115ca55d8699568
DINKAR202 <Dinkar202@outlook.com>
2023-11-18 22:03:54 +0530

    updated commit

--- login/client/src/components/Signup/index.jsx
+++ login/client/src/components/Signup/index.jsx
@@ -1,8 +1,29 @@
 import styles from './styles.module.css';
+import {Link} from 'react-router-dom';
 
 const Signup = () =>{
     return(
-
+            <div className={styles.signup_container}>
+                <div className={styles.signup_form_container}>
+                    <div className={styles.left}>
+                    <h1>Welcome Back</h1>   
+                    <Link to="/login">
+                        <button type='button' className={styles.white_btn}>
+                            Signup
+                        </button>
+                    </Link> 
+                    </div>
+                    <div className={styles.right}>
+                      <form className={styles.form_container}>
+                            <h1>Create Account</h1>
+                            <input type="text" 
+                            placeholder='' 
+                            id="" 
+                            />
+                        </form>          
+                    </div>
+                </div>
+            </div>
     )
 }
 
