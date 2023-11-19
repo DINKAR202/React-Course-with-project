eb8e4a1a0cd69fdb7d4a3d89066746db33f9c9ff
DINKAR202 <Dinkar202@outlook.com>
2023-11-20 01:05:04 +0530

    updated commit

--- CRUD/index.js
+++ CRUD/index.js
@@ -13,6 +13,9 @@ const MONGOURL = process.env.MONGO_URL;
 
 mongoose.connect(MONGOURL).then(()=>{
 
-    
+    console.log("DB connected successfully! ");
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
 
 }).catch(error => console.log(error))
