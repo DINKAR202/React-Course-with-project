30c53dc765a1c3df140e2894d8392faba9412b18
DINKAR202 <Dinkar202@outlook.com>
2023-11-30 22:54:50 +0530

    updated commit

--- CRUD/model/userModel.js
+++ CRUD/model/userModel.js
@@ -5,4 +5,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    email:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
 })
+
+export default mongoose.model("user")
