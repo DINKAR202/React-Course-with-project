73ed10af1e0e9713d18c7f09540a0f7a2e7256ec
DINKAR202 <Dinkar202@outlook.com>
2024-01-09 01:33:58 +0530

    updated project

--- MERN2024/server/utils/db.js
+++ MERN2024/server/utils/db.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
 
 const URI = "mongodb+srv://rathordinkar5:fZXuurOaX85aODc4@cluster0.8c7yjyg.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(URI)
+// mongoose.connect(URI)
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(URI);
+        console.log("Connection successful to DB");
+    } catch (error) {
+        console.log("database connection failed");
+        process.exit(0);
+    }
+}
