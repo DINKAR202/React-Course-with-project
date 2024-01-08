847eef7713acc351ac763d0a6dc45a8dee6affe3
DINKAR202 <Dinkar202@outlook.com>
2024-01-08 16:02:59 +0530

    updated project

--- MERN2024/server/router/auth-router.js
+++ MERN2024/server/router/auth-router.js
@@ -5,16 +5,21 @@ router.get("/", (req, res)=>{
     res.status(200).send("Welcome to Home page Using Router");
 });
 
-router.get("/register", (req, res)=>{
-    res.status(200).send("Welcome to Register page Using Router");
-});
+// router.get("/register", (req, res)=>{
+//     res.status(200).send("Welcome to Register page Using Router");
+// });
+
+router.route("/register").get((req, res) => {
+    res
+        .status(200)
+        .send("Welcome to Register page Using Router");
+})
 
 // This is second option of using Router
-
 router.route("/second").get((req, res) => {
     res
         .status(200)
         .send("This is second option of using Router");
 });
 
-module.exports = router
+module.exports = router;
