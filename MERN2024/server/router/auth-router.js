0d688b061c2d3820baff42bda2c644c767d26b76
DINKAR202 <Dinkar202@outlook.com>
2024-01-08 15:52:48 +0530

    updated project

--- MERN2024/server/router/auth-router.js
+++ MERN2024/server/router/auth-router.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.status(200).send("Welcome to Home page");
+    res.status(200).send("Welcome to Home page Using Router");
 });
 
-// app.get("/register", (req, res)=>{
-//     res.status(200).send("Welcome to Register page");
-// });
+router.get("/register", (req, res)=>{
+    res.status(200).send("Welcome to Register page Using Router");
+});
 
 module.exports = router
--- MERN2024/server/server.js
+++ MERN2024/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = require('./router/auth-router');
 
-app.use("/api/auth")
+app.use("/api/auth", router)
 
 
 const PORT = 5000;
