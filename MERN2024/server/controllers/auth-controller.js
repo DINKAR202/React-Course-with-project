daf5a0ed6ce1c16085fdcb062457c13185673a99
DINKAR202 <Dinkar202@outlook.com>
2024-01-09 13:14:31 +0530

    updated project

--- MERN2024/server/controllers/auth-controller.js
+++ MERN2024/server/controllers/auth-controller.js
@@ -17,9 +17,8 @@ const home = async (req, res) => {
 const register = async (req, res) => {
     try {
         console.log(req.body);
-        res
-            .status(200)
-            .json({message: req.body});
+        const data = req.body;
+        res.status(200).json({data});
     } catch (error) {
         res.status(500).json("internal server error")        
     }
--- MERN2024/server/models/user-model.js
+++ MERN2024/server/models/user-model.js
@@ -17,4 +17,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-})
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const User = new mongoose.model("User", userSchema);
+module.exports = User;
