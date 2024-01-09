b20472759e335e1fa315ea4c2bdd939cc54be6b1
DINKAR202 <Dinkar202@outlook.com>
2024-01-09 12:36:10 +0530

    updated project

--- MERN2024/server/models/user-model.js
+++ MERN2024/server/models/user-model.js
@@ -8,5 +8,13 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-    }
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
 })
