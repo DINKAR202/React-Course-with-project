68990c3667ba9b0d075266b674a52c233acdc08a
DINKAR202 <Dinkar202@outlook.com>
2024-01-09 20:37:09 +0530

    updated project

--- MERN2024/server/controllers/auth-controller.js
+++ MERN2024/server/controllers/auth-controller.js
@@ -1,48 +1,44 @@
 const User = require("../models/user-model");
 const bcrypt = require("bcryptjs");
 
-//Home logic 
+//Home logic
 
 const home = async (req, res) => {
-    try {
-        res
-        .status(200)
-        .send(
-            "Welcome to Home page Using Controllers This is priti here"
-        );
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    res
+      .status(200)
+      .send("Welcome to Home page Using Controllers This is priti here");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Register logic
 
 const register = async (req, res) => {
-    try {
-        console.log(req.body);
-        const {username, email, phone, password} = req.body;
-        const userExit = await User.findOne({email});
-        if(userExit) {
-            return res.status(400).json({msg: "email already exists"});
-        }
-
-        //Hash the password
-        // const saltRound = 10;
-        // const hash_password = await bcrypt.hash(password, saltRound);
-
-        const userCreated = await User.create({ username, email, phone, password});
-
-        res.status(200).json({msg: userCreated });
-    } catch (error) {
-        res.status(500).json("internal server error")        
+  try {
+    console.log(req.body);
+    const { username, email, phone, password } = req.body;
+    const userExit = await User.findOne({ email });
+    if (userExit) {
+      return res.status(400).json({ msg: "email already exists" });
     }
-}
+
+    //Hash the password
+    // const saltRound = 10;
+    // const hash_password = await bcrypt.hash(password, saltRound);
+
+    const userCreated = await User.create({ username, email, phone, password });
+
+    // Adding Token function JWT
+    res.status(201).json({ msg: userCreated, token: await userCreated.generateToken() });
+  } catch (error) {
+    res.status(500).json("internal server error");
+  }
+};
 
 const login = async (req, res) => {
-    res
-        .status(200)
-        .send("This is login page using Controllers");
-}
+  res.status(201).send("This is login page using Controllers");
+};
 
-
-module.exports = {home, register, login};
+module.exports = { home, register, login };
--- MERN2024/server/models/user-model.js
+++ MERN2024/server/models/user-model.js
@@ -36,11 +36,15 @@ userSchema.pre('save', async function(){
     }
     try {
         const saltRound = await bcrypt.genSalt(10);
-        const hash_password = await bcrypt.hash(password, saltRound);
+        const hash_password = await bcrypt.hash(user.password, saltRound);
+        user.password = hash_password;
     } catch (error) {
         next(error)
-    }
-})
+    };
+});
+
+// json web token
+userSchema.methods.generateToken = function () {};
 
 // Define the model or the collection name
 const User = new mongoose.model("User", userSchema);
