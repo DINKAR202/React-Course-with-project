7b30ba6c0869f38a0e80e43f440418312f74e391
DINKAR202 <Dinkar202@outlook.com>
2024-01-13 21:01:41 +0530

    updated project

--- MERN2024/server/controllers/contact-controller.js
+++ MERN2024/server/controllers/contact-controller.js
@@ -2,13 +2,13 @@ const Contact = require("../models/contact-model");
 
 const contactForm = async (req, res) => {
     try {
-        const response = req.body;
-        await Contact.create(response);
-        return res.status(200).json({ message: "Message send successfully"});
+      const response = req.body;
+      await Contact.create(response);
+      return res.status(200).json({ message: "message send successfully" });
     } catch (error) {
-        return res.status(500).json({ message: "Message not delivered" });
+      console.error(error);
+      return res.status(500).json({ message: "message not delivered" });
     }
-}
-
-
-module.exports = contactForm;
+  };
+  
+  module.exports = contactForm;
