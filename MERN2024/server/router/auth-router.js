2b3e62734e1bed4a5f400a69164d124919fd6198
DINKAR202 <Dinkar202@outlook.com>
2024-01-13 12:09:27 +0530

    updated project

--- MERN2024/server/router/auth-router.js
+++ MERN2024/server/router/auth-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
 const signupSchema = require("../validators/auth-validator");
-const validate = required("../middlewares/validate-middleware")
+const validate = require("../middlewares/validate-middleware")
 
 router.route("/").get(authcontrollers.home);
 router.route("/register").post(validate(signupSchema), authcontrollers.register);
