5111a82edf6718465e0e33ed4f2176dea261d4e7
DINKAR202 <Dinkar202@outlook.com>
2024-01-15 01:33:27 +0530

    updated project

--- MERN2024/client/src/pages/Home.jsx
+++ MERN2024/client/src/pages/Home.jsx
@@ -42,8 +42,23 @@ const Home = () => {
       {/* <Analytics /> */}
 
       <section className="section-analytics">
-        <div className="container grid grid-fourcols">
-          
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>registered companies</p>
+          </div>
+          <div className="div1">
+            <h2>100,00+</h2>
+            <p>Happy clients</p>
+          </div>
+          <div className="div1">
+            <h2>500+</h2>
+            <p>Well know Developer</p>
+          </div>
+          <div className="div1">
+            <h2>24/7</h2>
+            <p>service</p>
+          </div>
         </div>
       </section>
 
