1d5698b3f5e14ede9c2a564c5842fa19f5356406
DINKAR202 <Dinkar202@outlook.com>
2024-01-15 01:56:57 +0530

    updated project

--- MERN2024/client/src/pages/About.jsx
+++ MERN2024/client/src/pages/About.jsx
@@ -1,7 +1,46 @@
 const About = () => {
   return (
     <>
-      
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>We Dinkar Developer!</p>
+              <h1>Why Choose Us?</h1>
+              <p>
+                Expertice: Our team consists of experienced IT professionals who are passoniate about staying up-to-date with the latest industry trends.
+              </p>
+              <p>
+                Customization: We understood that every business is unique. That's why we create solutions that are tailored to your specific needs and goals.
+              </p>
+              <p>
+                Expertice: Our team consists of experienced IT professionals who are passoniate about staying up-to-date with the latest industry trends.
+              </p>
+              <p>
+                Expertice: Our team consists of experienced IT professionals who are passoniate about staying up-to-date with the latest industry trends.
+              </p>
+              <div className="btn btn-group">
+                <a href="/contact">
+                  <button className="btn">connect now</button>
+                </a>
+                <a href="/services">
+                  <button className="btn secondary-btn">learn more</button>
+                </a>
+              </div>
+            </div>
+
+            {/* hero images  */}
+            <div className="hero-image">
+              <img
+                src="/images/about.png"
+                alt="coding together"
+                width="400"
+                height="500"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
