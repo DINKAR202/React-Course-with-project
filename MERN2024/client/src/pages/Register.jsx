7745b965d344b636a0382c3ac8e85fd40c2ef20b
DINKAR202 <Dinkar202@outlook.com>
2024-01-15 15:25:28 +0530

    updated project

--- MERN2024/client/src/pages/Register.jsx
+++ MERN2024/client/src/pages/Register.jsx
@@ -23,18 +23,26 @@ const Register = () => {
     };
 
     // Handling the form submission
-     const handleSubmit = (e) => {
+     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(user);
+        try {
+          
         
-        const response = fetch(`http://localhost:5000/api/auth/register`, {
+        
+        const response = await fetch(`http://localhost:5000/api/auth/register`, {
           method: "POST",
           headers: {
             'Content-Type': "application/json",
           },
           body: JSON.stringify(user),
-        })
-     }
+        });
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+     };
+     
 
   return (
     <>
