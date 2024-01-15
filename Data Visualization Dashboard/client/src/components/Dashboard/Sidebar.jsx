e94b10796154215dd0ffee76e26818984728646e
DINKAR202 <Dinkar202@outlook.com>
2024-01-16 01:31:04 +0530

    updated project

--- Data Visualization Dashboard/client/src/components/Dashboard/Sidebar.jsx
+++ Data Visualization Dashboard/client/src/components/Dashboard/Sidebar.jsx
@@ -118,11 +118,11 @@ const AdminDashboard = () => {
             <Flex alignItems="center" mt="140%">
               <Avatar
                 size="lg"
-                src="https://avatars.githubusercontent.com/u/110231091?s=400&u=dfefa66e38cbdd84674e1a9f55737a51827b8725&v=4"
+                // src="https://avatars.githubusercontent.com/u/110231091?s=400&u=dfefa66e38cbdd84674e1a9f55737a51827b8725&v=4"
                 mr={4}
               />
               <Box>
-                <Heading size="md">Ritesh Kr. Shukla</Heading>
+                <Heading size="md">Dinkar Kr.</Heading>
                 <Text
                   fontSize="sm"
                   color={useColorModeValue("gray.500", "gray.400")}
