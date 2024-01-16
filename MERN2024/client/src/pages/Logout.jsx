c967eb8aacb83a0e0abbadc52d3ecc25fc6ca76f
DINKAR202 <Dinkar202@outlook.com>
2024-01-17 01:46:29 +0530

    updated project

--- MERN2024/client/src/pages/Logout.jsx
+++ MERN2024/client/src/pages/Logout.jsx
@@ -1,9 +1,13 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
+import {Navigate} from "react-router-dom";
 
 const Logout = () => {
   useEffect(() => {
-    
-  })
-}
+    LogoutUser();
+  }, [LogoutUser]);
 
-export default Logout
+
+  return <Navigate to="/login" />
+};
+
+export default Logout;
