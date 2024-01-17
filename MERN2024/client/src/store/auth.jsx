fc7fd693c395a8026a4af172cbe8ce8e0575c5ce
DINKAR202 <Dinkar202@outlook.com>
2024-01-17 14:36:54 +0530

    updated project

--- MERN2024/client/src/store/auth.jsx
+++ MERN2024/client/src/store/auth.jsx
@@ -30,7 +30,12 @@ export const AuthProvider = ({ children }) => {
 
   const userAuthentication = async () => {
     try {
-      
+      const response = await fetch("http://localhost:5000/api/auth/user", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
     } catch (error) {
       console.error("Error fetching user data!");
     }
