d911f1b16a837b4715005645f1d6aaf470acf571
DINKAR202 <Dinkar202@outlook.com>
2024-01-17 13:20:41 +0530

    updated project

--- MERN2024/server/middlewares/auth-middleware.js
+++ MERN2024/server/middlewares/auth-middleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
 
 const authMiddleware = async (req, res, next) => {
   const token = req.header("Authorization");
@@ -13,7 +14,15 @@ const authMiddleware = async (req, res, next) => {
 
   try {
     const isVerified = jwt.verify(jwtToken, process.env.JWT_SECRET_KEY);
-    console.log(isVerified);
+
+    const userData = await User.findOne({ email: isVerified.email }).select({
+      password: 0,
+    });
+    console.log(userData);
+
+    req.user = userData;
+    req.token = token;
+    req.userID = userData._id;
 
     next();
   } catch (error) {
