2bc3ccf404ea2be750a48f873a34ae9a7bd84a5b
DINKAR202 <Dinkar202@outlook.com>
2024-01-18 19:19:52 +0530

    updated project

--- MERN2024/server/controllers/service-controller.js
+++ MERN2024/server/controllers/service-controller.js
@@ -2,7 +2,7 @@ const Service = require("../models/service-model");
 
 const services = async (req, res) =>{
     try {
-        
+        const response = await Service.find();
     } catch (error) {
         console.log(`services ${error}`);
     }
