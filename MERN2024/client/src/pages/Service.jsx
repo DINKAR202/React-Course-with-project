992e24eed3f1a5a912c3cb6168ae5c4e6861a07e
DINKAR202 <Dinkar202@outlook.com>
2024-01-18 19:57:09 +0530

    updated project

--- MERN2024/client/src/pages/Service.jsx
+++ MERN2024/client/src/pages/Service.jsx
@@ -1,7 +1,22 @@
 const Service = () => {
   return (
-    <div>Service</div>
-  )
-}
+    <section className="section-services">
+      <div className="container">
+        <h1 className="main-heading">Services</h1>
+      </div>
+      <div className="container grid grid-three-cols">
+        <div className="card">
+          <div className="card-img">
+            <img src="/images/design.png" alt="design" width="500" />
+          </div>
 
-export default Service
+          <div className="card-details">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Service;
