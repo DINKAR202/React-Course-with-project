2bced684157994d103d4dbd4de4da4b7dde57431
DINKAR202 <Dinkar202@outlook.com>
2024-01-21 02:08:51 +0530

    updated project

--- MERN2024/server/controllers/admin-controller.js
+++ MERN2024/server/controllers/admin-controller.js
@@ -3,8 +3,14 @@ const User = require("../models/user-model");
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).josn(users);
+    if (!users || users.length === 0) {
+      return res.status(404).josn({ message: "No users found" });
+    }
+    return res.status(200).josn(users);
   } catch (error) {
     next(error);
   }
 };
+
+
+module.exports = getAllUsers;
