407f9616279838688386db60cd53bd4058724c43
DINKAR202 <Dinkar202@outlook.com>
2024-01-21 02:11:59 +0530

    updated project

--- MERN2024/server/server.js
+++ MERN2024/server/server.js
@@ -5,6 +5,7 @@ const app = express();
 const authRoute = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
 const serviceRoute = require("./router/service-router");
+const adminRoute = require("./router/admin-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
@@ -23,6 +24,9 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
 app.use("/api/data", serviceRoute);
+
+// Let's define admin route
+app.use("/api/admin", adminRoute);
 
 app.use(errorMiddleware);
 
