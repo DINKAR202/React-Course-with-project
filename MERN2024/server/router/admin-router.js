48b9ac5d636fad8f9b772e0878258398f478e394
DINKAR202 <Dinkar202@outlook.com>
2024-01-21 18:26:11 +0530

    updated project

--- MERN2024/server/router/admin-router.js
+++ MERN2024/server/router/admin-router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/admin-controller");
 const authMiddleware = require("../middlewares/auth-middleware");
 
-router.route('/users').get(adminController.getAllUsers);
+router.route('/users').get(authMiddleware, adminController.getAllUsers);
 router.route('/contacts').get(adminController.getAllContacts);
 
 module.exports = router;
