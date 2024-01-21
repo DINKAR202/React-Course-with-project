82c466a7cb4a1a80a5b9cc17a61ab91b3d5b34ea
DINKAR202 <Dinkar202@outlook.com>
2024-01-22 02:23:07 +0530

    updated project

--- MERN2024/server/middlewares/admin-middleware.js
+++ MERN2024/server/middlewares/admin-middleware.js
@@ -1,13 +1,14 @@
 const adminMiddleware = async (req, res, next) => {
   try {
     console.log(req.data);
-    // res.status(200).json({ msg: req.user.isAdmin });
     const adminRole = req.user.isAdmin;
     if (!adminRole) {
       return res
         .status(403)
-        .json({ error: "Access denied. User is not an admin." });
+        .json({ message: "Access denied. User is not an admin." });
     }
+    // res.status(200).json({ msg: req.user.isAdmin });
+    next();
   } catch (error) {
     next(error);
   }
