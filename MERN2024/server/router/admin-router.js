1ce43a0846444a726430a4ace94b647813bc7aa9
DINKAR202 <Dinkar202@outlook.com>
2024-01-22 02:29:41 +0530

    updated project

--- MERN2024/server/router/admin-router.js
+++ MERN2024/server/router/admin-router.js
@@ -4,10 +4,16 @@ const adminController = require("../controllers/admin-controller");
 const authMiddleware = require("../middlewares/auth-middleware");
 const adminMiddleware = require("../middlewares/admin-middleware");
 
-router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
+router
+  .route("/users")
+  .get(authMiddleware, adminMiddleware, adminController.getAllUsers);
 
-router.route("/users/delete/:id").delete(authMiddleware, adminMiddleware, adminController.deleteUserById);
+router
+  .route("/users/delete/:id")
+  .delete(authMiddleware, adminMiddleware, adminController.deleteUserById);
 
-router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
+router
+  .route("/contacts")
+  .get(authMiddleware, adminMiddleware, adminController.getAllContacts);
 
 module.exports = router;
