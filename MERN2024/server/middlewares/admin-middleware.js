d42e87ed344a2c29d32701d083640a4e831a995c
DINKAR202 <Dinkar202@outlook.com>
2024-01-22 02:08:11 +0530

    updated project

---
+++ MERN2024/server/middlewares/admin-middleware.js
@@ -0,0 +1,10 @@
+const adminMiddleware = async (req, res, next) => {
+  try {
+    console.log(req.data);
+    res.status(200).json({ msg: req.user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = adminMiddleware;
--- MERN2024/server/router/admin-router.js
+++ MERN2024/server/router/admin-router.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin-controller");
 const authMiddleware = require("../middlewares/auth-middleware");
+const adminMiddleware = require("../middlewares/admin-middleware");
 
-router.route("/users").get(authMiddleware, adminController.getAllUsers);
+router.route("/users").get(authMiddleware, adminMiddleware, adminController.getAllUsers);
 
-router.route("/users/delete/:id").delete(authMiddleware, adminController.deleteUserById);
+router.route("/users/delete/:id").delete(authMiddleware, adminMiddleware, adminController.deleteUserById);
 
 router.route("/contacts").get(authMiddleware, adminController.getAllContacts);
 
