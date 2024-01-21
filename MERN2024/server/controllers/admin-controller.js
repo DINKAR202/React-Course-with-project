2b81ae893e03f6470b56ab096622aeb704ea7f3c
DINKAR202 <Dinkar202@outlook.com>
2024-01-21 22:26:35 +0530

    updated project

--- MERN2024/server/controllers/admin-controller.js
+++ MERN2024/server/controllers/admin-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user-model");
-const Contact = require('../models/contact-model');
+const Contact = require("../models/contact-model");
 
 // Get Users logics
 
@@ -18,29 +18,28 @@ const getAllUsers = async (req, res) => {
 
 // User Data delete logic
 
-const deleteUserById = async(req, res) => {
+const deleteUserById = async (req, res) => {
   try {
     const id = req.params.id;
+    await User.deleteOne({ _id: id });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 // Get Contacts logic
 
 const getAllContacts = async (req, res) => {
-    try {
-      const contacts = await Contact.find();
-      console.log("admin data", contacts);
-      if (!contacts || contacts.length === 0) {
-        return res.status(404).json({ message: "No users found" });
-      }
-      return res.status(200).json(contacts);
-    } catch (error) {
-      next(error);
+  try {
+    const contacts = await Contact.find();
+    console.log("admin data", contacts);
+    if (!contacts || contacts.length === 0) {
+      return res.status(404).json({ message: "No users found" });
     }
-  };
+    return res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-
-
-module.exports = {getAllUsers, getAllContacts, deleteUserById};
+module.exports = { getAllUsers, getAllContacts, deleteUserById };
