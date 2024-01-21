ed2654b63060de0c5b37d9b633e996b51b047f53
DINKAR202 <Dinkar202@outlook.com>
2024-01-22 02:20:48 +0530

    updated project

--- MERN2024/server/middlewares/admin-middleware.js
+++ MERN2024/server/middlewares/admin-middleware.js
@@ -1,7 +1,8 @@
 const adminMiddleware = async (req, res, next) => {
   try {
     console.log(req.data);
-    res.status(200).json({ msg: req.user.isAdmin });
+    // res.status(200).json({ msg: req.user.isAdmin });
+    const adminRole = req.user.isAdmin;
   } catch (error) {
     next(error);
   }
