647da40c272326b04936d1dce9e5e673ecf0a7f9
DINKAR202 <Dinkar202@outlook.com>
2024-01-21 21:04:55 +0530

    updated project

--- MERN2024/client/src/pages/Admin-Users.jsx
+++ MERN2024/client/src/pages/Admin-Users.jsx
@@ -4,7 +4,12 @@ const AdminUsers = () => {
     
     const getAllUsersData = async () => {
         try {
-            
+            const response = await fetch("http://localhost:5000/api/admin/users", {
+                method: "GET",
+                headers:{
+                    Authorization:
+                }
+            })
         } catch (error) {
             console.log(error)
         }
