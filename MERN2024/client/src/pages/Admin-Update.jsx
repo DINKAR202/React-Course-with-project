b385fe9b77d19cbd51096f9dc91fec8e586c60a6
DINKAR202 <Dinkar202@outlook.com>
2024-01-27 03:11:50 +0530

    updated project

--- MERN2024/client/src/pages/Admin-Update.jsx
+++ MERN2024/client/src/pages/Admin-Update.jsx
@@ -62,7 +62,7 @@ const AdminUpdate = () => {
       const response = await fetch(
         `http://localhost:5000/api/admin/users/update/${params.id}`,
         {
-          method: "DELETE",
+          method: "PATCH",
           headers: {
             Authorization: authorizationToken,
           },
