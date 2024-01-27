4a674e32247e2876bd1c1c67709cf6ab487d2d35
DINKAR202 <Dinkar202@outlook.com>
2024-01-27 22:48:49 +0530

    updated project

--- MERN2024/client/src/components/layouts/Admin-Layout.jsx
+++ MERN2024/client/src/components/layouts/Admin-Layout.jsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, Navigate } from "react-router-dom";
 import { FaUser, FaHome, FaRegListAlt } from "react-icons/fa";
 import { FaMessage } from "react-icons/fa6";
 import { useAuth } from "./../../store/auth";
@@ -6,6 +6,10 @@ import { useAuth } from "./../../store/auth";
 const AdminLayout = () => {
   const { user } = useAuth();
   console.log("admin layout", user);
+
+  if(!user.isAdmin){
+    return <Navigate to="/"/>
+  }
   return (
     <>
       <header>
