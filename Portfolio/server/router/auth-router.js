dd06f71fc58df66d391e5d5eac3db5512668cd76
DINKAR202 <Dinkar202@outlook.com>
2024-02-05 18:21:41 +0530

    updated project

---
+++ Portfolio/server/router/auth-router.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const authcontrollers = require("../controllers/auth-controller");
+const { signupSchema, loginSchema } = require("../validators/auth-validator");
+const validate = require("../middlewares/validate-middleware");
+const authMiddleware = require("../middlewares/auth-middleware");
+
+router.route("/").get(authcontrollers.home);
+router.route("/register").post(validate(signupSchema), authcontrollers.register);
+router.route("/login").post(validate(loginSchema), authcontrollers.login);
+
+router.route('/user').get(authMiddleware, authcontrollers.user);
+
+// This is second option of using Router
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.get("/", (req, res)=>{
+//     res.status(200).send("Welcome to Home page Using Router This is priti here");
+// });
+
+// router.get("/register", (req, res)=>{
+//     res.status(200).send("Welcome to Register page Using Router");
+// });
