65c02ae9e68b229cdff2297a9a2af46d8b76fde1
Dinkar Kumar <109248473+DINKAR202@users.noreply.github.com>
2024-02-10 15:45:10 +0530

    Update server.js

--- Portfolio/server/server.js
+++ Portfolio/server/server.js
@@ -25,6 +25,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/form", contactRoute);
 app.use("/api/data", serviceRoute);
 //Got it
+//success
 
 // Let's define admin route
 app.use("/api/admin", adminRoute);
