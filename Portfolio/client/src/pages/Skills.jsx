29af0923b59575b0f7438a87b98b689db7e166fb
DINKAR202 <Dinkar202@outlook.com>
2024-02-16 16:18:32 +0530

    updated project

--- Portfolio/client/src/pages/Skills.jsx
+++ Portfolio/client/src/pages/Skills.jsx
@@ -3,11 +3,13 @@ const Skills = () => {
     <>
       <main>
         <section className="container">
-          <h1 className="main-heading">Skills</h1>
+          <div className="progress-under">
+            <h1 className="main-heading">Skills</h1>
 
-          <div className="boxes">
-            <div className="upper-loading">
-              <div className="Loading"></div>
+            <div className="boxes">
+              <div className="upper-loading">
+                <div className="Loading"></div>
+              </div>
             </div>
           </div>
         </section>
