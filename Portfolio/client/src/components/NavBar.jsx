80110a95921340df0a8ee1edad5cf4e28037f77f
DINKAR202 <Dinkar202@outlook.com>
2024-02-16 15:41:49 +0530

    updated project

--- Portfolio/client/src/components/NavBar.jsx
+++ Portfolio/client/src/components/NavBar.jsx
@@ -1,29 +1,20 @@
 // import { NavLink }   from "react-router-dom";
-import { useEffect } from 'react';
 import "./Navbar.css";
-// import "./Navbar.js";
-// import { useAuth } from "../store/auth";
 
 const NavBar = () => {
   // const { isLoggedIn } = useAuth();
-  useEffect(() => {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const nav = document.querySelector("nav");
-
-    const toggleNav = () => {
-      nav.classList.toggle("active");
-    };
-
-    if (menuToggle && nav) {
-      menuToggle.addEventListener("click", toggleNav);
-    }
-
-    return () => {
-      if (menuToggle && nav) {
-        menuToggle.removeEventListener("click", toggleNav);
-      }
-    };
-  }, []); // Empty dependency array ensures effect runs only once after initial render
+  const menu = () => {
+    document.addEventListener("DOMContentLoaded", function () {
+      const menuToggle = document.querySelector(".menu-toggle");
+      const nav = document.querySelector("nav");
+    
+      // Toggle navigation display when the button is clicked
+      menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
+      });
+    });
+    
+  };
 
   return (
     <>
@@ -57,7 +48,9 @@ const NavBar = () => {
           </nav>
 
           {/* <!-- Button to toggle navigation display --> */}
-          <button className="menu-toggle">Menu</button>
+          <button onClick={menu} className="menu-toggle">
+            Menu
+          </button>
         </div>
       </header>
     </>
