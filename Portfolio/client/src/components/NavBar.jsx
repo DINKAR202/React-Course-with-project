f1315f974c5ddb1f15b9b4cfc90b0f95c8d293f5
DINKAR202 <Dinkar202@outlook.com>
2024-02-16 15:12:33 +0530

    updated project

--- Portfolio/client/src/components/NavBar.jsx
+++ Portfolio/client/src/components/NavBar.jsx
@@ -1,57 +1,57 @@
-// import { NavLink } from "react-router-dom";
-import "./Navbar.css";
-// import { useAuth } from "../store/auth";
+import { useEffect } from 'react';
+import './Navbar.css';
 
 const NavBar = () => {
-  // const { isLoggedIn } = useAuth();
-
-  document.addEventListener('DOMContentLoaded', function() {
+  useEffect(() => {
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('nav');
 
     // Toggle navigation display when the button is clicked
-    menuToggle.addEventListener('click', function() {
+    const toggleNav = () => {
       nav.classList.toggle('active');
-    });
-  });
+    };
+
+    menuToggle.addEventListener('click', toggleNav);
+
+    return () => {
+      menuToggle.removeEventListener('click', toggleNav);
+    };
+  }, []);
 
   return (
-    <>
-      <header>
-  <div className="container">
-    <div className="logo-brand">
-      <a href="/">Portfolio.</a>
-    </div>
+    <header>
+      <div className="container">
+        <div className="logo-brand">
+          <a href="/">Portfolio.</a>
+        </div>
 
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/service">Services</a>
-        </li>
-        <li>
-          <a href="/service">Skills</a>
-        </li>
-        <li>
-          <a href="/service">Projects</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li> 
-      </ul>
-    </nav>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/service">Services</a>
+            </li>
+            <li>
+              <a href="/service">Skills</a>
+            </li>
+            <li>
+              <a href="/service">Projects</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li> 
+          </ul>
+        </nav>
 
-    {/* <!-- Button to toggle navigation display --> */}
-    <button className="menu-toggle">Menu</button>
-  </div>
-</header>
-
-    </>
+        {/* <!-- Button to toggle navigation display --> */}
+        <button className="menu-toggle">Menu</button>
+      </div>
+    </header>
   );
 };
 
