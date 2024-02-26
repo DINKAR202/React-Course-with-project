0d0055e5bb787ffa098006bfc82d8166545e8730
DINKAR202 <Dinkar202@outlook.com>
2024-02-26 10:52:58 +0530

    updated project

---
+++ firstapp/src/app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Hi there</h1>
+      <h2>Okay its done</h2>
+      <h2>Okay its done</h2>
+      <h2>Okay its done</h2>
+      <h2>Okay its done</h2>
+      <h2>Okay its done</h2>
+    </main>
+  );
+}
