75009230736b4fb65ff087fca6fc7f914ebdd6e7
DINKAR202 <Dinkar202@outlook.com>
2024-02-26 11:08:50 +0530

    updated project

---
+++ firstapp/src/app/course/java/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const java = () => {
+  return (
+    <div>java page </div>
+  )
+}
+
+export default java
--- firstapp/src/app/products/page.jsx
+++ firstapp/src/app/products/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const products = () => {
   return (
-    <div>products page</div>
-  )
-}
+    <>
+      products page
+      <hr />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+        esse. Consequatur sint quia, eaque amet fugit facilis nihil in veniam
+        ratione. Alias rerum maxime perspiciatis officia error omnis a
+        recusandae. Ducimus dolore a ab tempore nulla corporis. Blanditiis,
+        obcaecati. Aspernatur quisquam alias possimus! Ex fugit adipisci culpa
+        fuga modi ducimus?
+      </p>
+    </>
+  );
+};
 
-export default products
+export default products;
