5c615a559b64ff6664f40ec378021bba7df70e6b
DINKAR202 <Dinkar202@outlook.com>
2024-04-01 23:34:43 +0530

    updated commit

---
+++ aceternity/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ThreeDCardDemo } from "./components/3d-Comp";
+import { HeroParallaxDemo } from "./components/Heroparallex";
+
+function App() {
+  return (
+    <section>
+      <HeroParallaxDemo />
+      <ThreeDCardDemo />
+    </section>
+  );
+}
+
+export default App;
