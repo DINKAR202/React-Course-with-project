24bbe133c1da9e3a5c3e441ff0671ccbc9d00710
DINKAR202 <Dinkar202@outlook.com>
2024-04-28 23:16:35 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/components/Navbar.js
+++ Nextjs project/get-me-a-chai/app/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='bg-blue-950 text-white flex justify-between items-center px-4 h-16'>
-        <div className='logo font-bold'>
+        <div className='logo font-bold flex justify-center items-center'>
           <img src="tea.gif" width={44} alt="" />
           <span>GetMeaChai!</span>
           </div>
