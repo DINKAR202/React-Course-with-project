dcc1457133620034863424851d22ec04406afa24
DINKAR202 <Dinkar202@outlook.com>
2024-04-28 23:51:14 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/components/Navbar.js
+++ Nextjs project/get-me-a-chai/app/components/Navbar.js
@@ -1,28 +1,34 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className='bg-blue-950 text-white flex justify-between items-center px-4 h-16'>
-        <div className='logo font-bold flex justify-center items-center'>
-          <Link href="/"><img src="tea.gif" width={36} alt="" />
-          <span>GetMeaChai!</span></Link>
-          </div>
-        <ul className='flex justify-between gap-4'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Sign up</li>
-            <li>Login</li>
-        </ul>
-
-        <div>
-          <Link href={"/login"}>
-          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Login</button>
-          </Link>
+    <nav className="bg-blue-950 text-white flex justify-between items-center px-4 h-16">
+      
+      <Link href="/">
+        <div className="logo font-bold flex justify-center items-center">
+          <img src="tea.gif" width={36} alt="" />
+          <span>GetMeaChai!</span>
         </div>
-    </nav>
-  )
-}
+      </Link>
 
-export default Navbar
+      <ul className="flex justify-between gap-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Sign up</li>
+        <li>Login</li>
+      </ul>
+
+      <div>
+        <Link href={"/login"}>
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+            Login
+          </button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
