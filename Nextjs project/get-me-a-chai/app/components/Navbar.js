b4c570bde27ad81b8c0632dc31373d1767b7a9f4
DINKAR202 <Dinkar202@outlook.com>
2024-04-28 23:21:50 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/components/Navbar.js
+++ Nextjs project/get-me-a-chai/app/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -14,6 +15,12 @@ const Navbar = () => {
             <li>Sign up</li>
             <li>Login</li>
         </ul>
+
+        <div>
+          <Link href={"/login"}>
+          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Login</button>
+          </Link>
+        </div>
     </nav>
   )
 }
