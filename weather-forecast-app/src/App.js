3081a5495b3c6ebfbd9069185c8d8a04ee089492
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 02:13:51 +0530

    Under development

--- weather-forecast-app/src/App.js
+++ weather-forecast-app/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Inputs from './components/Inputs';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
+import TempratureandDetails from './components/TempratureandDetails';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-700">
       <TopButtons/>
       <Inputs/>
+      
       <TimeAndLocation/>
+      <TempratureandDetails/>
     </div>
   );
 }
---
+++ weather-forecast-app/src/components/TempratureandDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TempratureandDetails() {
+  return (
+    <div>
+      kjrsfd
+    </div>
+  )
+}
+
+export default TempratureandDetails
