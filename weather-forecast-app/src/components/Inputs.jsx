ed1517691bdb6b4e55a57dfb1f4b6f5cb0855dd7
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 01:39:44 +0530

    Under development

--- weather-forecast-app/src/components/Inputs.jsx
+++ weather-forecast-app/src/components/Inputs.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { UilSearchAlt, UilLocationPoint } from '@iconscout/react-unicons'
+import React from "react";
+import { UilSearchAlt, UilLocationPoint } from "@iconscout/react-unicons";
 
 function Inputs() {
   return (
     <>
-    <div className='flex flex-row justify-center my-6'>
-    <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
-        <input type="text" className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize' />
-    </div>
-    </div>
+      <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+          <input
+            type="text"
+            placeholder="Search for city ..."
+            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Inputs
+export default Inputs;
