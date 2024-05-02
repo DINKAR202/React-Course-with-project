68d3faef3392c5fa5697d3653385fe9defe6a60e
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 01:11:20 +0530

    Under development

--- weather-forecast-app/src/components/TopButtons.jsx
+++ weather-forecast-app/src/components/TopButtons.jsx
@@ -8,25 +8,31 @@ const TopButtons = () => {
             title: 'london'
         },
         {
-            id:1,
-            title: 'london'
+            id:2,
+            title: 'Sydney'
         },
         {
-            id:1,
-            title: 'london'
+            id:3,
+            title: 'Tokyo'
         },
         {
-            id:1,
-            title: 'london'
+            id:4,
+            title: 'Toronto'
         },
         {
-            id:1,
-            title: 'london'
+            id:5,
+            title: 'Paris'
         },
     ]
 
   return (
-    <div>TopButtons</div>
+    <div className='flex items-center justify-content my-6'>
+        {
+            cities.map((city)=>(
+                <button className='text-white text-lg font-medium'>{city.title}</button>
+            ))
+        }
+    </div>
   )
 }
 
