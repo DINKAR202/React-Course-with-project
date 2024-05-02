c812a198204adc39c4b322fec184e8585ad18f4b
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 00:59:21 +0530

    Under development

--- weather-forecast-app/src/App.js
+++ weather-forecast-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hii there</h1>
+      <h1 className='font-2xl font-bold'>Hii there</h1>
+      <UilReact/>
     </div>
   );
 }
