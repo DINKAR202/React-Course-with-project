752d419628442c831da45414e89789a0bdf0bec5
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 05:00:24 +0530

    Under development

--- weather-forecast-app/src/App.js
+++ weather-forecast-app/src/App.js
@@ -9,9 +9,11 @@ import getFormattedWeatherData from './services/weatherService';
 function App() {
 
   const fetchWeather = async () => {
-    const data =await getFormattedWeatherData("weather", {q:"london"});
-    console.log(data)
+    const data = await getFormattedWeatherData({q: "london" });
+    console.log("weather data", data)
   }
+
+  fetchWeather();
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-700">
