3ae15eec900590348ee3960a67c340c2c10d6238
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 02:12:28 +0530

    Under development

--- weather-forecast-app/src/components/TimeAndLocation.jsx
+++ weather-forecast-app/src/components/TimeAndLocation.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function TimeAndLocation() {
   return (
     <>
-     <div className='flex items-center justify-center my-6'>
-        <p className='text-white text-xl font-extralight'>
-        Tuesday, 31 May 2022 | Local time: 12:36 PM
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 31 May 2022 | Local time: 12:36 PM
         </p>
-        
-        </div> 
+      </div>
+
+      
     </>
-  )
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
