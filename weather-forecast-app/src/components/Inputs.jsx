c7fcd07f028fa39dbd98f38f7c1045136e4e080c
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 01:38:20 +0530

    Under development

--- weather-forecast-app/src/components/Inputs.jsx
+++ weather-forecast-app/src/components/Inputs.jsx
@@ -5,7 +5,9 @@ function Inputs() {
   return (
     <>
     <div className='flex flex-row justify-center my-6'>
-    sekfj
+    <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
+        <input type="text" className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize' />
+    </div>
     </div>
     </>
   )
