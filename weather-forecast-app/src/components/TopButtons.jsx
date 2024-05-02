c54bd817d8c9daf5b3135902161501693592d52c
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 01:05:32 +0530

    Under development

--- weather-forecast-app/src/components/TopButtons.jsx
+++ weather-forecast-app/src/components/TopButtons.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
 const TopButtons = () => {
+
+    const cities = [
+        {
+            id:1,
+            title: 'london'
+        },
+        {
+            id:1,
+            title: 'london'
+        },
+        {
+            id:1,
+            title: 'london'
+        },
+        {
+            id:1,
+            title: 'london'
+        },
+        {
+            id:1,
+            title: 'london'
+        },
+    ]
+
   return (
     <div>TopButtons</div>
   )
