2178751cf0d3918704cd2ce029cc106f80c6c4f1
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 03:34:49 +0530

    Under development

--- weather-forecast-app/src/components/Forecast.jsx
+++ weather-forecast-app/src/components/Forecast.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 function Forecast() {
   return (
     <>
-     <div className='flex items-center justify-center mt-6'>
-        <p className='text-white text-start font-medium uppercase'>
+     <div className='flex items-center justify-start mt-6'>
+        <p className='text-white font-medium uppercase'>
             hourly forecast
         </p>
         </div>
         <hr className='my-2'/> 
+
+        <div className='flex flex-row items-center justify-between text-white'>
+            <div className='flex flex-col items-center justify-center'>
+                <p className='font-light text-sm'>04:30 PM</p>
+            </div>
+        </div>
     </>
   )
 }
