bbf0defd268cf46344bb0cb168e07a246fcec028
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 01:03:52 +0530

    Under development

--- weather-forecast-app/src/App.js
+++ weather-forecast-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TopButtons from './components/TopButtons';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-700">
-      
+      <TopButtons/>
     </div>
   );
 }
