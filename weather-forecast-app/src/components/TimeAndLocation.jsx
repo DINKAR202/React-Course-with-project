e7f04668c735100a948f627a47e1415d7d598dab
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 02:02:11 +0530

    Under development

--- weather-forecast-app/src/components/TimeAndLocation.jsx
+++ weather-forecast-app/src/components/TimeAndLocation.jsx
@@ -5,8 +5,9 @@ function TimeAndLocation() {
     <>
      <div className='flex items-center justify-center my-6'>
         <p className='text-white text-xl font-extralight'>
-
+        Tuesday, 31 May 2022 | Local time: 12:36 PM
         </p>
+        
         </div> 
     </>
   )
