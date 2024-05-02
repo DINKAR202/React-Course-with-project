abeec7049cd9f86c3773d9ca7c679a57a9f30470
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 04:18:33 +0530

    Under development

--- weather-forecast-app/src/services/weatherService.js
+++ weather-forecast-app/src/services/weatherService.js
@@ -2,3 +2,11 @@ const API_KEY = "2e0d000e702c4105890caba964ef6ad4";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL + "/" + infoType);
+    url.search = new URLSearchParams({...searchParams, apiid:API_KEY});
+
+    return fetch(url)
+    .then((res) => res.json())
+}
