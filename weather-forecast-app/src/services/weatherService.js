e39066949b08f491c306dcb94f75302042b6aebe
DINKAR202 <Dinkar202@outlook.com>
2024-05-03 03:58:22 +0530

    Under development

---
+++ weather-forecast-app/src/services/weatherService.js
@@ -0,0 +1,4 @@
+const API_KEY = "2e0d000e702c4105890caba964ef6ad4";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
