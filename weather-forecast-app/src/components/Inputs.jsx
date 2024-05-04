eb5ddffcac3cbdcf2b647e783f09f1f610d5d174
DINKAR202 <Dinkar202@outlook.com>
2024-05-05 02:39:33 +0530

    Under development

--- weather-forecast-app/src/components/Inputs.jsx
+++ weather-forecast-app/src/components/Inputs.jsx
@@ -4,12 +4,32 @@ import { UilSearchAlt, UilLocationPoint } from "@iconscout/react-unicons";
 function Inputs({setQuery, units, setUnits}) {
 
   const [city, setCity] = useState("")
+  
+  const handleSearchClick = () => {
+    if (city != '') setQuery({q:city})
+  }
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) =>{
+        let lat = position.coords.latitude;
+        let lon = position.coords.latitude;
+
+        setQuery({
+          lat,
+          lon
+        })
+      })
+    }
+  }
 
   return (
     <>
       <div className="flex flex-row justify-center my-6">
         <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
           <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
             type="text"
             placeholder="Search for city ..."
             className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
@@ -18,6 +38,7 @@ function Inputs({setQuery, units, setUnits}) {
           <UilSearchAlt
             size={25}
             className="text-white cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleSearchClick}
           />
           <UilLocationPoint
             size={25}
