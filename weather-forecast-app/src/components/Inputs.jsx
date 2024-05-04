0ce7dc9244d16866f7689d4b4da19ff5585ba1af
DINKAR202 <Dinkar202@outlook.com>
2024-05-05 02:45:21 +0530

    Under development

--- weather-forecast-app/src/components/Inputs.jsx
+++ weather-forecast-app/src/components/Inputs.jsx
@@ -1,35 +1,39 @@
 import React, { useState } from "react";
 import { UilSearchAlt, UilLocationPoint } from "@iconscout/react-unicons";
 
-function Inputs({setQuery, units, setUnits}) {
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState("");
 
-  const [city, setCity] = useState("")
-  
-  const handleSearchClick = () => {
-    if (city != '') setQuery({q:city})
+  const handleUnitsChange = (e) => {
+    const selectedUnit = e.currentTarget.name;
+    if(units !== selectedUnit) setUnits(selectedUnit);
   }
+
+  const handleSearchClick = () => {
+    if (city != "") setQuery({ q: city });
+  };
 
   const handleLocationClick = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) =>{
+      navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude;
         let lon = position.coords.latitude;
 
         setQuery({
           lat,
-          lon
-        })
-      })
+          lon,
+        });
+      });
     }
-  }
+  };
 
   return (
     <>
       <div className="flex flex-row justify-center my-6">
         <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
           <input
-          value={city}
-          onChange={(e) => setCity(e.currentTarget.value)}
+            value={city}
+            onChange={(e) => setCity(e.currentTarget.value)}
             type="text"
             placeholder="Search for city ..."
             className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
@@ -48,13 +52,21 @@ function Inputs({setQuery, units, setUnits}) {
         </div>
 
         <div className="flex flex-row w-1/4 items-center justify-center">
-            <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125">
+          <button
+            name="metric"
+            className="text-xl text-white font-light transition ease-out hover:scale-125"
+            onClick="handleUnitsChange"
+          >
             &deg;C
-            </button>
-            <p className="text-xl text-white mx-1">|</p>
-            <button name="metric" className="text-xl text-white font-light  transition ease-out hover:scale-125">
+          </button>
+          <p className="text-xl text-white mx-1">|</p>
+          <button
+            name="imperial"
+            className="text-xl text-white font-light  transition ease-out hover:scale-125"
+            onClick="handleUnitsChange"
+          >
             &deg;F
-            </button>
+          </button>
         </div>
       </div>
     </>
