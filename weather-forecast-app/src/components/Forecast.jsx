6a4e321764e2eeb8449c0eafca0bf371fb08e7c3
DINKAR202 <Dinkar202@outlook.com>
2024-05-05 01:55:12 +0530

    Under development

--- weather-forecast-app/src/components/Forecast.jsx
+++ weather-forecast-app/src/components/Forecast.jsx
@@ -18,42 +18,6 @@ function Forecast({title}) {
           />
           <p className="font-medium">22&deg;</p>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-            alt=""
-          />
-          <p className="font-medium">22&deg;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-            alt=""
-          />
-          <p className="font-medium">22&deg;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-            alt=""
-          />
-          <p className="font-medium">22&deg;</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <p className="font-light text-sm">04:30 PM</p>
-          <img
-            src="http://openweathermap.org/img/wn/01d@2x.png"
-            className="w-12 my-1"
-            alt=""
-          />
-          <p className="font-medium">22&deg;</p>
-        </div>
       </div>
     </>
   );
