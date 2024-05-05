10d83705d6a8160c1aac8e09bbdd62a177764b08
DINKAR202 <Dinkar202@outlook.com>
2024-05-06 00:38:27 +0530

    Under development

--- weather-forecast-app/src/components/Inputs.jsx
+++ weather-forecast-app/src/components/Inputs.jsx
@@ -36,7 +36,7 @@ function Inputs({ setQuery, units, setUnits }) {
             onChange={(e) => setCity(e.currentTarget.value)}
             type="text"
             placeholder="Search for city ..."
-            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+            className="text-xl rounded-lg font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
           />
 
           <UilSearchAlt
