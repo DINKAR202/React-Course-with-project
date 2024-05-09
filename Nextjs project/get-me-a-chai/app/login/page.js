c92f30d8c096dcdec20277531237ecee099d646a
DINKAR202 <Dinkar202@outlook.com>
2024-05-10 01:15:14 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/login/page.js
+++ Nextjs project/get-me-a-chai/app/login/page.js
@@ -6,7 +6,8 @@ const Login = () => {
     <div className='text-white py-14 container mx-auto'>
       <h1 className='font-bold text-3xl text-center'>Signup to Get fans to support you</h1>
       <div className='social-login-buttons'>
-     <div className="flex flex-col gap-2 min-h-screen bg-gray-100 p-10">
+
+     <div className="flex flex-col gap-2 min-h-screen items-center p-10">
   <button className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="-0.5 0 48 48" version="1.1">
       <g id="Icons" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
