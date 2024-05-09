f7ecb0f70272253fdf689fb65f472e59f4a52d77
DINKAR202 <Dinkar202@outlook.com>
2024-05-10 01:30:07 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/components/Navbar.js
+++ Nextjs project/get-me-a-chai/app/components/Navbar.js
@@ -4,6 +4,14 @@ import { useSession, signIn, signOut } from "next-auth/react"
 import Link from "next/link";
 
 const Navbar = () => {
+  const { data: session } = useSession()
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  
   return (
     <nav className="bg-blue-950 text-white flex justify-between items-center px-4 h-16">
       
