88e8aee984aa96c68b571b6aec6d9ffec06a2f3e
DINKAR202 <Dinkar202@outlook.com>
2024-05-10 02:30:40 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/components/Navbar.js
+++ Nextjs project/get-me-a-chai/app/components/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
 
       <div>
         <Link href={"/login"}>
-          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={()=>{signIn("github")}}>
+          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
             Login
           </button>
         </Link>
