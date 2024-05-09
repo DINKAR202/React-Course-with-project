3e928174d2ededcf394f89d5efc3ceaa6a50b5cc
DINKAR202 <Dinkar202@outlook.com>
2024-05-10 00:37:29 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/signup/page.js
+++ Nextjs project/get-me-a-chai/app/signup/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <div className='text-white py-14'>
+      <h1 className='font-bold text-3xl'>Login to Get fans to support you</h1>
+    </div>
+    </>
   )
 }
 
