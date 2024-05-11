87e81bbb396a82b1d25d4ce20e8cb0514ec7b406
DINKAR202 <Dinkar202@outlook.com>
2024-05-12 01:53:57 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/components/Navbar.js
+++ Nextjs project/get-me-a-chai/app/components/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
     </svg>
   </button>
 
-  <div id="dropdown" className={`z-10 ${showdropdown?"":"hidden"} absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+  <div id="dropdown" className={`z-10 ${showdropdown?"":"hidden"} absolute mx-32 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
     <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
       <li>
         <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
