76b97f2262177d38ac47851abb55d759d9f8bbd2
DINKAR202 <Dinkar202@outlook.com>
2024-05-12 23:56:25 +0530

    Under development

--- Nextjs project/get-me-a-chai/app/[username]/page.js
+++ Nextjs project/get-me-a-chai/app/[username]/page.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Username = ({params}) => {
   return (
-    <>
-    <div className='text-white'>
+    <div className="text-white">
         {params.username}
+    <div className='w-full cover'>
+        <img className="object-cover" src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/4842667/452146dcfeb04f38853368f554aadde1/eyJ3IjoxNjAwLCJ3ZSI6MX0%3D/16.gif?token-time=1717459200&token-hash=b5fK345liFbxiFSjKhhIL7-xnygYT7EXrRWE65Zgf1Q%3D"
+        alt=""
+        />
     </div>
-    </>
+    </div>
   )
 }
 
