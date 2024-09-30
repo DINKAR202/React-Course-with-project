ec56fd188f5f2b72dc0132260569e43782372b8b
DINKAR202 <Dinkar202@outlook.com>
2024-09-30 17:01:44 +0530

    DINKAR DEV

--- Ecommerce/ecommerce/src/app/(auth)/signup/page.jsx
+++ Ecommerce/ecommerce/src/app/(auth)/signup/page.jsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 
-import FormInput from '@/components/FormInput';
-import React from 'react'
+import FormInput from "@/components/FormInput";
+import React from "react";
 
 const Signup = () => {
-
-    const onSubmit = async () => {};
+  const onSubmit = async () => {};
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
       <div className="h-screen">
@@ -14,11 +13,13 @@ const Signup = () => {
       <div className="p-[15%] bg-white">
         <h1>Create an account</h1>
         <form action={onSubmit}>
-            <FormInput/>
+          <FormInput id="name" label="Full Name" placeholder="Enter your Name" type="text" className="h-10" />
+          <FormInput id="email" label="Email" placeholder="Enter your Email" type="email" className="h-10" />
+          <FormInput id="password" label="Password" placeholder="Enter the Password" type="text" className="h-10" />
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
