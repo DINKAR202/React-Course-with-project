fc97ab9da602519a7b756c9eecdfb19247f4ddc1
DINKAR202 <Dinkar202@outlook.com>
2024-09-30 11:25:12 +0530

    Under development

--- Ecommerce/ecommerce/src/components/Header.jsx
+++ Ecommerce/ecommerce/src/components/Header.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Link from 'next/link'
+import React from "react";
+import Navbar from "./Navbar";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div>
         <Link href="/">
-        <img src="/logo.png" className="h-12 cursor-pointer" alt="" />
+          <img src="/logo.png" className="h-12 cursor-pointer" alt="" />
         </Link>
+        <div>
+            <Link href='/' className="hidden sm:block">Home</Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
