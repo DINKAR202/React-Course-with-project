0ebabf9c8b560296617566a33efd2defbdaab854
DINKAR202 <Dinkar202@outlook.com>
2024-11-15 19:49:27 +0530

    DINKAR DEV

--- PracticeInterview/notessaver/src/redux/pasteSlice.js
+++ PracticeInterview/notessaver/src/redux/pasteSlice.js
@@ -1,29 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = {
-  value: 0,
-}
-
-export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+export const pasteSlice = createSlice({
+  name: 'pastes',
+  initialState: {
+    value: 0,
+  },
   reducers: {
     increment: (state) => {
       state.value += 1
-    },
-    reset: (state) => {
-      state.value = 0
     },
     decrement: (state) => {
       state.value -= 1
     },
     incrementByAmount: (state, action) => {
-      state.value += Number(action.payload)
+      state.value += action.payload
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount, reset } = counterSlice.actions
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
 export default counterSlice.reducer
