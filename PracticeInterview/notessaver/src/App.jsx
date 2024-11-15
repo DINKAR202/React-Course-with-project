ed814afecb840cc94f2f311842b6a5103e7124c1
DINKAR202 <Dinkar202@outlook.com>
2024-11-15 20:51:13 +0530

    DINKAR DEV

--- PracticeInterview/notessaver/src/App.jsx
+++ PracticeInterview/notessaver/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import createBrowserRouter from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Paste from "./components/Paste";
+import ViewPaste from "./components/ViewPaste";
 
 const router = createBrowserRouter([
   {
@@ -13,11 +15,18 @@ const router = createBrowserRouter([
   },
   {
     path: "/pastes",
-    element: <div></div>,
+    element: <div>
+      <Navbar/>
+      <Paste/>
+    </div>,
   },
   {
     path: "/pastes/:id",
-    element: <div></div>,
+    element: <div>
+      <Navbar/>
+      <ViewPaste/>
+
+    </div>,
   },
 ]);
 
