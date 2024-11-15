a65861aa47997d4bb25767cca8812eaaf7b4ec05
DINKAR202 <Dinkar202@outlook.com>
2024-11-15 09:57:37 +0530

    DINKAR DEV

--- PracticeInterview/redux/src/main.jsx
+++ PracticeInterview/redux/src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ApiProvider } from "./../node_modules/@reduxjs/toolkit/src/query/react/ApiProvider";
+import { Provider } from 'react-redux'
 import { store } from "./redux/store.js";
 import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ApiProvider store={store}>
+    <Provider store={store}>
       <App />
-    </ApiProvider>
+    </Provider>
   </StrictMode>
 );
