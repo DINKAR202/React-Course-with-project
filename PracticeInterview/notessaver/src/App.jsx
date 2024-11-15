a93a4ce29e02e55c482ae2c32e61de47b73aaeb8
DINKAR202 <Dinkar202@outlook.com>
2024-11-15 20:49:31 +0530

    DINKAR DEV

--- PracticeInterview/notessaver/src/App.jsx
+++ PracticeInterview/notessaver/src/App.jsx
@@ -1,25 +1,32 @@
-import './App.css'
-import createBrowserRouter from 'react-router-dom';
+import "./App.css";
+import createBrowserRouter from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <div>
-
-      </div>
-    },
-  ]
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      <Navbar/>
+      <Home/>
+    </div>,
+  },
+  {
+    path: "/pastes",
+    element: <div></div>,
+  },
+  {
+    path: "/pastes/:id",
+    element: <div></div>,
+  },
+]);
 
 function App() {
-
   return (
     <>
-      <h1 className='font-bold'>HI</h1>
+      <h1 className="font-bold">HI</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
