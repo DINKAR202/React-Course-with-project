c778056669c255c3e05afd2338042e91807cd7ac
DINKAR202 <Dinkar202@outlook.com>
2024-11-18 13:50:55 +0530

    DINKAR DEV

--- PracticeInterview/index.js
+++ PracticeInterview/index.js
@@ -2,11 +2,23 @@
 
 //  console.log(age)
 
-function sayHello() {
-    var name = "Sab thik hain yrr tm batao";
-    console.log(name)
+// function sayHello() {
+//     var name = "Sab thik hain yrr tm batao";
+//     console.log(name)
+// }
+
+// console.log(name)
+
+// sayHello();
+
+// console.log(name)
+
+
+//     var name = "Sab thik hain yrr tm batao";
+
+
+function sayName(myName="DINKAR DEV"){
+    console.log("My name is:", myName)
 }
 
-console.log(name)
-
-sayHello();
+sayName("D");
