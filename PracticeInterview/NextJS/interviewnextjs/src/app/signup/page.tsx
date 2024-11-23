5c88c281960c9e4b2228d6f7116d1d400644b6ce
DINKAR202 <Dinkar202@outlook.com>
2024-11-23 16:53:07 +0530

    DINKAR DEV

--- PracticeInterview/NextJS/interviewnextjs/src/app/signup/page.tsx
+++ PracticeInterview/NextJS/interviewnextjs/src/app/signup/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { Link } from 'next/link';
 
 export default function page() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
   return (
-    <div>
-Signup      
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>SignUp</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="username">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="username">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+
+      <button onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+      >SignUp Here</button>
+
+      <Link href="/loign">Visit login page</Link>
     </div>
-  )
+  );
 }
