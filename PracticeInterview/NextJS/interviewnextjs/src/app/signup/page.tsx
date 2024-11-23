18cc4b9d54032fa1e7aad1c32a1634c182a7f4cd
DINKAR202 <Dinkar202@outlook.com>
2024-11-23 22:07:09 +0530

    DINKAR DEV

--- PracticeInterview/NextJS/interviewnextjs/src/app/signup/page.tsx
+++ PracticeInterview/NextJS/interviewnextjs/src/app/signup/page.tsx
@@ -2,7 +2,8 @@
 
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function page() {
   const router = useRouter();
@@ -14,8 +15,21 @@ export default function page() {
   });
 
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
-  const onSignup = async () => {};
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup Success", response.data);
+      router.push("/login");
+      
+    } catch (error: any) {
+      console.log("Sign up Error", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (
@@ -31,7 +45,7 @@ export default function page() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>SignUp</h1>
+      <h1>{loading ? "Processing" : "Signup"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
@@ -65,7 +79,7 @@ export default function page() {
         onClick={onSignup}
         className="p-2 text-white border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
       >
-        SignUp Here
+        {buttonDisabled ? "No signup" : "Signup"}
       </button>
 
       <Link href="/login">Visit login page</Link>
