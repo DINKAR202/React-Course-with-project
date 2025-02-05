79be3be121b773d40f3785b0db7047f82ade9884
DINKAR202 <Dinkar202@outlook.com>
2025-02-06 01:26:15 +0530

    DINKAR DEV

--- PracticeInterview/AdvanceLevelBackend/server.js
+++ PracticeInterview/AdvanceLevelBackend/server.js
@@ -10,7 +10,16 @@ app.get("/", (req, res) => {
     res.json({ message: "Hi, it's working fine..." });
 });
 
-// Start the server with error handling
+// * Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//* import routes
+
+import ApiRoutes from "./routes/api.js";
+app.use("/api", ApiRoutes);
+
+//* Start the server with error handling
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }).on("error", (err) => {
